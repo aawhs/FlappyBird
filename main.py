2ed4919f2536176a200507a3b78d7b543580799e
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-07-11 05:00:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((576,1024))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('resources/images/background-day.png')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(bg_surface , (0,0))
+    pygame.display.update()
+    clock.tick(120)
